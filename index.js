162f171fd94b0f046eb2b79ccc897aefaa2a1db9
Shweta Yadav <sry9281@gmail.com>
2024-12-26 18:24:13 +0530

    added express middleware

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const routes = require('./route');
 
 const app = express();
 const port = 3000;
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
 app.listen(port, () => {
     console.log('Server listening on port:', port);
